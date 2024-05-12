eecf26eb09f976e6ecc9f740f981258f7e7a18a5
SammyBits <samuelbeato7@gmail.com>
2024-05-12 19:29:10 -0400

    Actualizar carga lazy  para imágenes en el componente Sections

---
+++ src/types/services/Barbers.ts
@@ -0,0 +1,12 @@
+type Barbers = {
+  uuid?: string;
+  firstName: string;
+  lastName: string;
+  userUUID: string;
+  specialty: string[];
+  phone: string;
+  description: string;
+  photo?: string;
+  createdAt?: string;
+  status?: boolean;
+};
