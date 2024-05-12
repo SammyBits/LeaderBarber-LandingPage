60861cf8f66fdfd17f24e52eceded26f94820791
SammyBits <samuelbeato7@gmail.com>
2024-05-12 00:01:16 -0400

    feat: Agregar servicios de envío de correos electrónicos
    Se añadieron los servicios de envío de correos electrónicos al proyecto. El archivo EmailServices.ts proporciona una función para enviar correos electrónicos en formato HTML utilizando la API de un servicio externo. Estos cambios son necesarios para implementar la funcionalidad de envío de correos electrónicos necesaria para el registro de usuarios.

--- src/services/auth/AuthServices.ts
+++ src/services/auth/AuthServices.ts
@@ -1,15 +1,22 @@
 import { BASEAPI_URL } from "../../utils/BaseSettings";
-
-export const registerClientGoogle = async (user: ClientGoogle) => {
+type response = {
+  message: string;
+  result: boolean;
+};
+export const registerClientGoogleIfNotExists = async (user: ClientGoogle) => {
   try {
-    const response = await fetch(`${BASEAPI_URL}/api/v1/clientgoogle`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(user),
-    });
-    return await response.json();
+    const existingClient = await isClientGoogle(user.email as string);
+    if (!existingClient) {
+      const response = await fetch(`${BASEAPI_URL}/api/v1/clientgoogle`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+      const { result }: response = await response.json();
+      return result;
+    }
   } catch (error) {
     console.error(error);
   }
@@ -18,7 +25,8 @@ export const registerClientGoogle = async (user: ClientGoogle) => {
 export const isClientGoogle = async (email: string) => {
   try {
     const response = await fetch(`${BASEAPI_URL}/api/v1/clientgoogle/${email}`);
-    return await response.json();
+    const { message, result }: response = await response.json();
+    return result;
   } catch (error) {
     console.error(error);
   }
