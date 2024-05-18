5fcd75dc1b8380ed9f319b18152420b2abd3d4a2
SammyBits <samuelbeato7@gmail.com>
2024-05-18 19:00:33 -0400

    feat: Improve appointment loading time and code readability
    chore: Rename some sections to English
    
    feat: Add Offers section
    
    feat: Add Toast notification for appointment confirmation
    
    chore: Remove Modal component
    
    feat: Set cookie on appointment confirmation for displaying Toast on index
    
    feat: Create new folder for SVG icons
    
    chore: Remove console.logs
    
    feat: Add local information
    
    chore: Update lazy loading for barber options in citas component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,19 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import auth from "auth-astro";
 import node from "@astrojs/node";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-
   i18n: {
     locales: ["en-US", "es-ES"],
-    defaultLocale: "en-US",
+    defaultLocale: "en-US"
   },
-  integrations: [tailwind(), auth()],
+  integrations: [tailwind(), auth(), react()],
   adapter: node({
-    mode: "standalone",
-  }),
+    mode: "standalone"
+  })
 });
